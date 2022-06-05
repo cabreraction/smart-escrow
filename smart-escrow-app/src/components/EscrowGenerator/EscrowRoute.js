3463e72535b4f41d971ef576672985e860b2a72b
Javier Cabrera <cabreraction@gmail.com>
2022-06-05 08:38:49 -0500

    added redux

--- smart-escrow-app/src/components/EscrowGenerator/EscrowRoute.js
+++ smart-escrow-app/src/components/EscrowGenerator/EscrowRoute.js
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+
 function EscrowRoute({ index }) {
+  const [ responseAttributesCount, setResponseAttributesCount ] = useState(0);
+
+  const addResponseAttribute = () => {
+    setResponseAttributesCount(prev => prev + 1);
+  }
+
+  const renderAttributes = () => {
+    const attributes = [];
+    for (let i = 0; i < responseAttributesCount; i++) {
+      const attributeView = (
+        <div>
+          I am an attribute!!
+        </div>
+      );
+      attributes.push(attributeView);
+    }
+
+    return attributes;
+  }
+
   return (
     <div className='container my-3'>
       <div className='row my-4'>
         <div className='col-sm-6'>
-          <label className='mx-1'>Ruta</label>
+          <label className='mx-1'>Endpoint</label>
           <input 
             type='text' 
             className='form-control' 
@@ -12,7 +34,7 @@ function EscrowRoute({ index }) {
           />
         </div>
         <div className='col-sm-6'>
-          <label className='mx-1'>Verbo</label>
+          <label className='mx-1'>Operaci&oacute;n</label>
           <select 
             className='form-select' 
             onChange={null}
@@ -27,8 +49,16 @@ function EscrowRoute({ index }) {
       <div className="row my-3">
         <div className="d-flex justify-content-between">
           <h3>Atributos de la respuesta</h3>
-          <button className="btn btn-outline-primary">Agregar +</button>
+          <button 
+            className="btn btn-outline-primary" 
+            onClick={addResponseAttribute}
+          >
+            Agregar +
+          </button>
         </div>
+      </div>
+      <div className='row my-3'>
+        {renderAttributes()}
       </div>
     </div>
   );
