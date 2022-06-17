1574db75abeb097a6c4d00f1252d5fe2eba18f05
Javier Cabrera <cabreraction@gmail.com>
2022-06-17 07:41:56 -0500

    started with escrow validations

--- smart-escrow-app/src/components/EscrowGenerator/Validators/ServiceValidation.js
+++ smart-escrow-app/src/components/EscrowGenerator/Validators/ServiceValidation.js
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
+
 import Main from "../../Main/Main";
+import ProcessInfo from '../../ProcessInfo/ProcessInfo'
+import { escrowValidatorsGenerationGuide } from '../../../content/content';
 
 function ServiceValidation() {
   return (
     <Main>
-      Validations
+      <div className='d-flex flex-column mx-2'>
+        <div className='container mb-4'>
+          <h1 className='mb-4'>Validaciones del Fideicomiso</h1>
+          <ProcessInfo 
+            { ...escrowValidatorsGenerationGuide } 
+          />
+        </div>
+      </div>
     </Main>
   );
 }
--- smart-escrow-app/src/content/content.js
+++ smart-escrow-app/src/content/content.js
@@ -21,3 +21,17 @@ export const escrowDetailsGenerationGuide = {
       del array puede tener, si la respuesta es un objeto entonces son los atributos que ese objeto debe tener al ser recibido.`
   ]
 };
+
+export const escrowValidatorsGenerationGuide = {
+  title: 'Fideicomiso: Validaciones',
+  summary: `Cuando el desarrollador entregue el producto finalizado este debe poder ser evaluado para determinar si cumple con las caracteristicas especificadas originalmente. 
+  Con este fin debe incluir una serie de pruebas para cada ruta y operación definida para el fideicomiso. Por favor tome en cuenta las siguientes observaciones:`,
+  steps: [
+    `Las validaciones se realizaran empezando con todas aquellas que hagan uso de la operación POST.`,
+    `Las validaciones de tipo GET pueden y deberían realizarse después de cada operación de tipo POST, PUT o DELETE, así que al definirla deberá especificar después de qué 
+    operación debe ser realizada.`,
+    `Para cada ruta y operación debe definir cuál es el código de respuesta esperado (200, 404, etc).`,
+    `Que las validaciones sean definidas correctamente es su responsabilidad, si estas no son exitosas el producto no será liberado.`,
+    `Dada la naturaleza de los fideicomisos, este no puede cambiar su información una vez haya sido iniciado, si necesitan modificar especificaciones deberán crear un fideicomiso nuevo.`
+  ]
+};
